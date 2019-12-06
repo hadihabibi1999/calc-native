72f401eeb366a2cfa5ea4bda677530bd0a0a0ba8
HadiCyb <hadihabibi2000@gmail.com>
2019-12-06 18:48:18 +0330

    stopwatch has been finished

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,25 +1,24 @@
+
 import React, {Component} from 'react';
 import { ScrollView, View,  Text, StyleSheet} from 'react-native';
 import moment from 'moment';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-  const Data = {
-    time:12345,
-    laps:[1123, 4566, 23456, 43456]
-  }
-
+  
   function Timer({interval, style}) {
+    const pad = (n) => n < 10 ? '0'+n : n
     const duration = moment.duration(interval)
     const centisecond = Math.floor(duration.milliseconds()/10)
-    return <Text style={style}>{duration.minutes()}:{duration.seconds()}:{centisecond}</Text>
+    return <Text style={style}>{pad(duration.minutes())}:{pad(duration.seconds())}.{pad(centisecond)}</Text>
   }
 
-  function RoundButtons ({title,color,backgroundColor}){
+  function RoundButtons ({title,color,backgroundColor,onPress,disabled}){
   return (
-    <View style={[styles.button,{backgroundColor:backgroundColor}]}> 
+    <TouchableOpacity onPress={()=>!disabled && onPress() } activeOpacity={disabled ? 0.1 : 0.6} style={[styles.button,{backgroundColor:backgroundColor}]}> 
       <View style={styles.buttonBorder}>
         <Text style={[styles.buttonTitle,{color}]}>{title}</Text>
       </View>
-    </View>
+    </TouchableOpacity>
    )
   }
 
@@ -44,7 +43,7 @@ import moment from 'moment';
     )
   }
 
-  function LapsTable ({laps}){
+  function LapsTable ({laps,timer}){
     const finishedLap = laps.slice(1)
     let max = Number.MIN_SAFE_INTEGER
     let min = Number.MAX_SAFE_INTEGER
@@ -60,7 +59,7 @@ import moment from 'moment';
         {laps.map((lap,index)=>(
           <Lap 
             number={laps.length - index} 
-            interval={lap}
+            interval={index===0?timer+lap:lap}
             key={laps.length - index}
             slowest={lap===max}
             fastest={lap===min}
@@ -70,16 +69,100 @@ import moment from 'moment';
   }
 
 export default class LinksScreen extends Component {
- 
+ constructor(props){
+   super(props);
+   this.state={
+    start:0,
+    now:0,
+    laps:[ ]
+   }
+ }
+
+ componentWillUnmount(){
+   clearInterval(this.timer)
+ }
+
+
+ start=()=>{
+const now = new Date().getTime()
+this.setState({
+start:now,
+now,
+laps:[0]
+})
+this.time = setInterval(()=>{
+  this.setState({
+    now : new Date().getTime()})
+  },100)
+ }
+
+
+ lap=()=>{
+   const timestamp = new Date().getTime()
+   const {laps,now,start} = this.state
+   const [firstLap, ...others] = laps
+   this.setState({
+   laps :[0,firstLap+now-start,...others],
+   start:timestamp,
+   now:timestamp
+   })
+ }
+
+ stop=()=>{
+   clearInterval(this.time)
+  const {laps,now,start} = this.state
+  const [firstLap, ...others] = laps
+  this.setState({
+  laps :[firstLap + now - start ,  ...others],
+  start:0,
+  now:0
+  })
+ }
+
+ reset=()=>{
+   this.setState({
+     laps:[],
+     now:0,
+     start:0
+   })
+ }
+  
+ resume=()=>{
+   const now = new Date().getTime()
+   this.setState({
+     start:now,
+     now,
+   })
+   this.time = setInterval(()=>{
+    this.setState({
+      now : new Date().getTime()})
+    },100)
+ }
+
+
   render(){
+    const {now,start,laps} = this.state
+    const time = now - start
     return (
       <View style={styles.container}>
-        <Timer interval={Data.time} style={styles.timer}/>
-        <ButtonsRow>
-          <RoundButtons title='Reset' color='#FFFFFF' backgroundColor='#3D3D3D'/>
-          <RoundButtons title='Start' color='#50D167' backgroundColor='#1B361F'/>
-        </ButtonsRow>
-          <LapsTable laps={Data.laps}/>
+        <Timer interval={laps.reduce((total,curr)=>total+curr,0)+time} style={styles.timer}/>
+
+        {laps.length===0 && (  <ButtonsRow>
+          <RoundButtons onPress={this.reset} title='Lap' color='#8B8B90' backgroundColor='#151515' disabled/>
+          <RoundButtons onPress={this.start} title='Start' color='#50D167' backgroundColor='#1B361F'/>
+        </ButtonsRow>)}
+       
+        {start > 0 && ( <ButtonsRow>
+          <RoundButtons onPress={this.lap}   title='Lap' color='#FFFFFF' backgroundColor='#3D3D3D'/>
+          <RoundButtons onPress={this.stop} title='Stop' color='#E33935' backgroundColor='#3C1715'/>
+        </ButtonsRow>)}
+
+        {laps.length > 0 && start===0 && ( <ButtonsRow>
+          <RoundButtons onPress={this.reset}   title='Reset' color='#FFFFFF' backgroundColor='#3D3D3D'/>
+          <RoundButtons onPress={this.resume} title='Start'  color='#50D167' backgroundColor='#1B361F'/>
+        </ButtonsRow>)}
+       
+          <LapsTable laps={laps} timer={time}/>
       </View>
     );
   }
@@ -116,7 +199,7 @@ const styles = StyleSheet.create({
       width:78,
       height:78,
       borderRadius:40,
-      borderWidth:2,
+      borderWidth:1,
       justifyContent:'center',
       alignItems:'center',
     },
